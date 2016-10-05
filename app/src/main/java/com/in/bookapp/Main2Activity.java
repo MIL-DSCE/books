4d5d81549e02035de25c2ead814984dd67bc2292
RASHI <rashiv0702@gmail.com>
2016-10-05 18:58:28 +0530

    First

---
+++ app/src/main/java/com/in/bookapp/Main2Activity.java
@@ -0,0 +1,231 @@
+package com.in.bookapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Demonstrate Firebase Authentication using a custom minted token. For more information, see:
+ * https://firebase.google.com/docs/auth/android/custom-auth
+ */
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "CustomAuthActivity";
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+    private String mCustomToken;
+//    private TokenBroadcastReceiver mTokenReceiver;
+    Button bt;
+    Button btn;
+    Button rg_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        findViewById(R.id.register_button).setOnClickListener(this);
+        rg_btn = (Button) findViewById(R.id.register_button);
+
+
+        rg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, SignupActivity.class));
+            }
+        });
+
+
+
+        // Button click listeners
+        findViewById(R.id.button_sign_in).setOnClickListener(this);
+
+        bt = (Button)findViewById(R.id.button_list);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, Main3Activity.class));
+            }
+        });
+
+//        // Create token receiver (for demo purposes only)
+//        mTokenReceiver = new TokenBroadcastReceiver() {
+//            @Override
+//            public void onNewToken(String token) {
+//                Log.d(TAG, "onNewToken:" + token);
+//                setCustomToken(token);
+//            }
+//        };
+
+
+        //Updating the database
+        findViewById(R.id.database_button).setOnClickListener(this);
+
+        btn = (Button) findViewById(R.id.database_button);
+
+
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Aryan");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
+    }
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        // [START_EXCLUDE]
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        // [START_EXCLUDE]
+//        unregisterReceiver(mTokenReceiver);
+        // [END_EXCLUDE]
+    }
+    // [END on_stop_remove_listener]
+
+    private void startSignIn() {
+        // Initiate sign in with custom token
+        // [START sign_in_custom]
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCustomToken:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCustomToken", task.getException());
+                            Toast.makeText(Main2Activity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END sign_in_custom]
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            ((TextView) findViewById(R.id.text_sign_in_status)).setText(
+                    "User ID: " + user.getUid());
+        } else {
+            ((TextView) findViewById(R.id.text_sign_in_status)).setText(
+                    "Error: sign in failed.");
+        }
+    }
+
+    private void setCustomToken(String token) {
+        mCustomToken = token;
+
+        String status;
+        if (mCustomToken != null) {
+            status = "Token:" + mCustomToken;
+        } else
+        {
+            status = "Token: null";
+        }
+
+        // Enable/disable sign-in button and show the token
+        findViewById(R.id.button_sign_in).setEnabled((mCustomToken != null));
+        ((TextView) findViewById(R.id.text_token_status)).setText(status);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.button_sign_in) {
+            startSignIn();
+
+        }
+    }
+
+    private abstract class TokenBroadcastReceiver extends BroadcastReceiver {
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+
+        public abstract void onNewToken(String token);
+    }
+}
